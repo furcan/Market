d20c5dcc9ad8cbef5f9988e3f245d3a37bf24730
Furkan MT <49978011+furcan@users.noreply.github.com>
2021-10-23 02:19:43 +0300

    Heroku: test.

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,6 +1,36 @@
 function App(): JSX.Element {
+
+  const testOnClickAsync = async (): Promise<void> => {
+    try {
+      const request = await fetch('https://furcan.herokuapp.com/api/posts?_page=1&_limit=2', {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log('request: ', request);
+
+      const totalCount = request.headers.get('X-Total-Count');
+      console.log('totalCount: ', totalCount);
+
+      const response = await request.json();
+      console.log('response: ', response);
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <h1 data-testid="App">Market</h1>
+    <div data-testid="App">
+      <h1>Market</h1>
+      <button
+        type="button"
+        onClick={testOnClickAsync}
+      >
+        <span>TEST GET</span>
+      </button>
+    </div>
   );
 }
 
