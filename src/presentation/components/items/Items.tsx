748cd1478f52b2e6ae5a0552afe36a061f196d83
Furkan MT <49978011+furcan@users.noreply.github.com>
2021-10-24 15:24:38 +0300

    UI: Items, design and state changes.

--- src/presentation/components/items/Items.tsx
+++ src/presentation/components/items/Items.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { FiAlertTriangle as IconFailure } from 'react-icons/fi';
 
 import { constants } from 'application/constants';
 import { convertPriceToLocaleString } from 'application/helpers';
@@ -18,8 +19,9 @@ function Products(): JSX.Element {
     failureProductsItems,
     dataProductItems,
   } = useSelector(rdxProductsSelector);
+  const placeholder: string[] = Array(constants.api.productsLimit).fill('');
 
-  useEffect(() => { // TODO
+  useEffect(() => { // TODO, FILTERS WILL BE CHANGED
     if (filterType && filterSortOrder) {
       dispatch(rdcProductsItemsGetDataAsync(
         [filterType, filterSortOrder],
@@ -36,12 +38,15 @@ function Products(): JSX.Element {
   return (
     <div className={styles.items}>
       {
-        loadingProductItems && // TODO
-        <p>Loading</p>
+        loadingProductItems &&
+        placeholder.map((_, index) => <div key={index} className={[styles.items__single, styles['items__single--placeholder'] || ''].join(' ')}></div>)
       }
       {
-        failureProductsItems && // TODO
-        <p>Failure</p>
+        failureProductsItems &&
+        <div className={[styles.items__single, styles['items__single--failure'] || ''].join(' ')}>
+          <IconFailure className={styles['items__single--failure__icon'] || ''} />
+          <p className={styles['items__single--failure__message'] || ''}>{constants.text.common.failure}</p>
+        </div>
       }
       {
         (!loadingProductItems && !failureProductsItems && dataProductItems.length > 0) &&
@@ -51,7 +56,13 @@ function Products(): JSX.Element {
             className={styles.items__single}
           >
             <div className={styles.items__single__media}>
-              <img className={styles.items__single__media__image} width="100" height="100" alt="TODO" src={`data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAZAAAAGQAQMAAAC6caSPAAAAAXNSR0IB2cksfwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAANQTFRF/9APCGXRuQAAACpJREFUeJztwTEBAAAAwqD1T+1vBqAAAAAAAAAAAAAAAAAAAAAAAAAAgDdPsAABORaXeAAAAABJRU5ErkJggg==`} />
+              <img
+                className={styles.items__single__media__image}
+                width="100"
+                height="100"
+                src={`data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAZAAAAGQAQMAAAC6caSPAAAAAXNSR0IB2cksfwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAANQTFRF/9APCGXRuQAAACpJREFUeJztwTEBAAAAwqD1T+1vBqAAAAAAAAAAAAAAAAAAAAAAAAAAgDdPsAABORaXeAAAAABJRU5ErkJggg==`}
+                alt={constants.app.name}
+              />
             </div>
             <p className={styles.items__single__price}>
               <span className={styles.items__single__price__currency}>{constants.app.currency}</span>
@@ -60,6 +71,7 @@ function Products(): JSX.Element {
             <h3 className={styles.items__single__title}>{item.name}</h3>
             <button
               type="button"
+              onClick={() => alert('TODO: ADD TO BASKET => REDUX')}
               className={styles.items__single__button}
             >
               <span>{constants.text.products.buttonAdd}</span>
